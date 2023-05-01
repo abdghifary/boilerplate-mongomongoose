b00bee8420cfdb30a89dc1f5e6ff3812c3a0c567
Agi <abdghifary@gmail.com>
2023-05-01 22:08:53 +0700

    find people by id

--- myApp.js
+++ myApp.js
@@ -56,7 +56,11 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, (err, data) => {
+    if (err) return console.error(err);
+    console.log(data);
+    done(null, data);
+  });
 };
 
 const findEditThenSave = (personId, done) => {
