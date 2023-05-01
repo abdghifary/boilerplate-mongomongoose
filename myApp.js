e72b4f6b485ef99fd79e3066d17d740476193147
Agi <abdghifary@gmail.com>
2023-05-01 22:06:14 +0700

    find one people by food

--- myApp.js
+++ myApp.js
@@ -48,7 +48,11 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food }, (err, data) => {
+    if (err) return console.error(err);
+    console.log(data);
+    done(null, data);
+  });
 };
 
 const findPersonById = (personId, done) => {
